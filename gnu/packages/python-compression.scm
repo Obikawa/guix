a69602f538d4626dce665f8508c834542362ef03
Petr Hodina <phodina@protonmail.com>
2021-07-30 16:00:39 +0200

    gnu: Add python-zipstream-new.
    * gnu/packages/python-compression.scm (python-zipstream-new): New variable.

--- gnu/packages/python-compression.scm
+++ gnu/packages/python-compression.scm
@@ -401,6 +401,28 @@ wrapper.  It provides a backport of the @code{Path} object.")
         ("python-unittest2" ,python2-unittest2)
         ,@(package-native-inputs base))))))
 
+(define-public python-zipstream-new
+  (package
+    (name "python-zipstream-new")
+    (version "1.1.8")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (pypi-uri "zipstream-new" version))
+        (sha256
+          (base32
+            "0xhw24zhibmyypcdbmvql02jcf3npisb29lx71w1drcl3ccgwcdh"))))
+    (build-system python-build-system)
+    (native-inputs `(("python-nose" ,python-nose)
+                     ("python-markdown" ,python-markdown)))
+    (home-page
+      "https://github.com/arjan-s/python-zipstream")
+    (synopsis
+      "Zipfile generator")
+    (description
+      "Zipfile generator that takes input files as well as streams")
+    (license license:gpl3)))
+
 ;; This package is used to bootstrap pytest, via importlib-metadata.
 (define-public python2-zipp-bootstrap
   (hidden-package
