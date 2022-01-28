1c2d771f46c9a5b98fc8b2cce8737f8630bdf5d1
Petr Hodina <phodina@protonmail.com>
2022-01-28 10:35:53 +0100

    gnu: Add adcli.
    * gnu/packages/admin.scm (adcli): New variable.

--- gnu/packages/admin.scm
+++ gnu/packages/admin.scm
@@ -197,6 +197,38 @@ simplicity in mind.")
     (home-page "https://github.com/nomius/ktsuss")
     (license license:bsd-3)))
 
+; I/O error : Attempt to load network entity http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl
+(define-public adcli
+(package
+  (name "adcli")
+  (version "0.9.1")
+  (source (origin
+            (method git-fetch)
+            (uri (git-reference
+             (url "https://gitlab.freedesktop.org/realmd/adcli")
+             (commit version)))
+            (file-name (string-append name "-" version ".tar.gz"))
+            (sha256
+             (base32
+              "1mwzd5vakdsssdvs6vljqpp8pw8i97n5lhxvmn9dn9720am7hfv7"))))
+  (build-system gnu-build-system)
+  (native-inputs `(("pkg-config" ,pkg-config)
+                   ("autoconf" ,autoconf)
+                   ("libtool" ,libtool)
+                   ("cyrus-sasl" ,cyrus-sasl)
+                   ("xmlto" ,xmlto)
+                   ("libxslt" ,libxslt)
+                   ("docbook-xml" ,docbook-xml)
+                   ("docbook-xsl" ,docbook-xsl)
+                   ("automake" ,automake)))
+  (inputs `(("openldap" ,openldap)
+	    ("util-linux" ,util-linux) ; for rev
+            ("krb5" ,mit-krb5)))
+  (synopsis "Helper library and tools for Active Directory client operations")
+  (description "Helper library and tools for Active Directory client operations.")
+  (home-page "https://gitlab.freedesktop.org/realmd/adcli")
+  (license license:lgpl2.1)))
+
 (define-public aide
   (package
     (name "aide")
