1faf76b2307e4c6627a8aba8157981f250f80786
Brendan Tildesley <mail@brendan.scot>
2022-02-01 18:34:57 +1100

    gnu: extra-cmake-modules: Update to 5.90.0.
    * gnu/packages/kde-frameworks.scm (extra-cmake-modules): Update to 5.90.0.
    Disable failing test.
    We don't need to run the test phase after install anymore.
    [patches]: Fix reproducibility bug.
    
    * gnu/packages/patches/extra-cmake-modules-reproducible-tarballs.patch:
    New patch.
    * gnu/local.mk: Reference patch.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1024,6 +1024,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/elm-compiler-disable-reactor.patch	\
   %D%/packages/patches/elm-compiler-fix-map-key.patch		\
   %D%/packages/patches/elogind-revert-polkit-detection.patch	\
+  %D%/packages/patches/extra-cmake-modules-reproducible-tarballs.patch	\
   %D%/packages/patches/emacs-exec-path.patch			\
   %D%/packages/patches/emacs-fix-scheme-indent-function.patch	\
   %D%/packages/patches/emacs-ignore-empty-xim-styles.patch	\
