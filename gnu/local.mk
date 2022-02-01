ee3110b286cddd5cc0c000e53e7420ca05b180a5
Brendan Tildesley <mail@brendan.scot>
2022-02-02 01:12:06 +1100

    gnu: kinit: Update to 5.90.0
    * gnu/packages/kde-frameworks.scm (kinit): Update to 5.90.0.
    * gnu/packages/patches/kinit-kdeinit-extra_libs.patch: Fix patch.
    * gnu/packages/patches/kinit-kdeinit-libpath.patch: Delete patch. The
    code this appies to was removed completely.
    * gnu/local.mk: Remove reference to libpath patch.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1355,7 +1355,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/kiki-missing-includes.patch		\
   %D%/packages/patches/kiki-portability-64bit.patch		\
   %D%/packages/patches/kinit-kdeinit-extra_libs.patch		\
-  %D%/packages/patches/kinit-kdeinit-libpath.patch		\
   %D%/packages/patches/kio-search-smbd-on-PATH.patch		\
   %D%/packages/patches/kmail-Fix-missing-link-libraries.patch \
   %D%/packages/patches/kmod-module-directory.patch		\
