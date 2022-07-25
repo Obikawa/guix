b9a56e26ce073af53d6c6f80e52e8a563986fd00
Petr Hodina <phodina@protonmail.com>
2022-07-25 11:40:42 +0200

    gnu: kwindowsystem: Remove input labels.
    * gnu/packages/kde-frameworks.scm (kwindowsystem): Remove input labels.

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -1127,11 +1127,11 @@ configuration pages, message boxes, and password requests.")
            qttools
            xorg-server-for-tests)) ; for the tests
     (inputs
-     `(("libxrender" ,libxrender)
-       ("qtbase" ,qtbase-5)
-       ("qtx11extras" ,qtx11extras)
-       ("xcb-utils-keysyms" ,xcb-util-keysyms)
-       ("xcb-util-wm" ,xcb-util-wm)))
+     (list libxrender
+           qtbase-5
+           qtx11extras
+           xcb-util-keysyms
+           xcb-util-wm))
     (arguments
      `(#:phases
        (modify-phases %standard-phases
